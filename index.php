d0e3c44fd3fa101b15439ef9f0235537b2e40fd8
kawany <kawany.santos8@etec.sp.gov.br>
2023-12-08 10:44:39 -0300

    versão beta

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" type="text/css" href="css/estilo.css"/>
+
+</head>
+<body>
+    <section id="principal">
+        <?php
+           echo "bom dia";
+        ?>
+    </section>
+</body>
+</html>
